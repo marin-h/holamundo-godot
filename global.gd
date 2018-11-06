d5b333d7c1bc40d24f62053e2bfff8de51bd2bfd
Marina Huberman <marina@gcoop.coop>
2018-11-05 21:20:43 -0300

    Manejamos el ganar y perder desde la escena global, y mostramos el segundero  en el viewport

--- global.gd
+++ global.gd
@@ -1,10 +1,32 @@
 # global
 extends Node
 
+signal perdi
+signal gane
+
 export var cuenta = 0
+export var tiempo = 5
+
+var current_scene = null
 
 func _ready():
-	pass
-
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+	connect("gane", current_scene, "show_ganar", [], CONNECT_ONESHOT)
+	connect("perdi", current_scene, "show_perder", [], CONNECT_ONESHOT)
+	get_node("Timer").start()
+	get_node("Timer").connect("timeout", current_scene, "decrementar_reloj", [], CONNECT_PERSIST)
+	
 func sumarUno():
 	cuenta = cuenta + 1
+	if(cuenta == 6):
+		get_node("Timer").queue_free()
+		emit_signal("gane")
+
+func _on_Timer_timeout():
+	tiempo = tiempo - 1
+	if(tiempo > 0):
+		get_node("Timer").start()
+	else: 
+		get_node("Timer").queue_free()
+		emit_signal("perdi")
